f72ef20c1677a9ba76550e42472be8dd90be5bbb
PeterTheMango <pyeshuajs23@gmail.com>
2022-05-02 23:52:20 +0300

    Revert "latest version"
    This reverts commit a83c80110d8544c1bcae18928e295bdcf8939bdd.

--- index.js
+++
@@ -1,8 +0,0 @@
-// npm i discord.js ms moment moment-duration-format glob util path colors
-
-const {
-    init
-} = require(`./Structures/Client`);
-const Config = require(`./Assets/Config.json`);
-
-init(Config);
